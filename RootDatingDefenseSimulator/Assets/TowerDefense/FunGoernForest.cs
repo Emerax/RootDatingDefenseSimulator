cc11c9c07eef96f1146a9654c4f176b2ebb54cda
Erik Mansén <e.naaru@hotmail.com>
2023-02-04 11:29:26 +0100

    Add main forest

---
+++ RootDatingDefenseSimulator/Assets/TowerDefense/FunGoernForest.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class FunGoernForest : MonoBehaviour {
+    [SerializeField]
+    private float maxHealth;
+
+    private Health health;
+
+    private Action onDeath;
+
+    public void Init(Action gameOverListener) {
+        health = GetComponent<Health>();
+        health.Init(maxHealth);
+        health.SetHealthListener(CheckDeath);
+        SetDeathListener(gameOverListener);
+    }
+
+    public void SetDeathListener(Action onDeath) {
+        this.onDeath += onDeath;
+    }
+
+    private void CheckDeath(float health, float maxHealth) {
+        if(health <= 0) {
+            onDeath?.Invoke();
+        }
+    }
+}
