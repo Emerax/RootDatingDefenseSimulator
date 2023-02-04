7a2705bc4337296201cab6e7636dc7875e8fdcb4
valwa <valdemar.wadstromer@gmail.com>
2023-02-04 21:44:18 +0100

    Trees can date

--- RootDatingDefenseSimulator/Assets/DatingUI/DatingHandler.cs
+++ RootDatingDefenseSimulator/Assets/DatingUI/DatingHandler.cs
@@ -23,6 +23,11 @@ public class DatingHandler : MonoBehaviour
     [SerializeField] private Button datePromptYesButton;
     [SerializeField] private Button datePromptNoButton;
 
+    [SerializeField] private GameObject dateMinigameParent;
+    [SerializeField] private DatingSpeechBubble datePromptBubble;
+    [SerializeField] private DatingSpeechBubble[] dateAnswerBubble;
+    private int correctAnswerBubbleIndex;
+
     private TreeDateStatblock[] trees;
     private enum DateState { SelectTree, StartDatePrompt, Date};
     private DateState dateState;
@@ -66,12 +71,24 @@ public class DatingHandler : MonoBehaviour
         for (int i = 0; i < selectedProfiles.Length; i++)
         {
             selectedProfiles[i].gameObject.SetActive(false);
+
+            //integerPointerExorcist explained in the for-loop above
+            int integerPointerExorcist = i;
+            selectedProfiles[i].selectTreeButton.onClick.AddListener(delegate { SelectedButtonDeselection(integerPointerExorcist); });
+        }
+
+        for (int i = 0; i < dateAnswerBubble.Length; i++)
+        {
+            //integerPointerExorcist explained in the for-loop above
+            int integerPointerExorcist = i;
+            dateAnswerBubble[i].button.onClick.AddListener(delegate { AnswerDatePrompt(integerPointerExorcist); });
         }
 
         datePromptYesButton.onClick.AddListener(TryDate);
 
         dateState = DateState.SelectTree;
         datePrompt.SetActive(false);
+        dateMinigameParent.SetActive(false);
     }
 
     /// <summary>
@@ -81,8 +98,9 @@ public class DatingHandler : MonoBehaviour
     /// <param name="tree"></param>
     public void SelectTree(int treeIndex)
     {
+        //TODO: Unlock tree image. Also do this when TD player takes them from us.
         //If tree already is selected, deselect
-        for(int i = 0; i < selectedProfiles.Length; i++)
+        for (int i = 0; i < selectedProfiles.Length; i++)
         {
             if (!selectedProfiles[i].gameObject.activeInHierarchy)
                 continue;
@@ -90,11 +108,15 @@ public class DatingHandler : MonoBehaviour
                 continue;
 
             selectedProfiles[i].gameObject.SetActive(false);
+
             profilePictures[treeIndex].Highlight(false);
+            //recover selected tree to list
+            profilePictures[treeIndex].gameObject.SetActive(true);
             ShouldShowDatePrompt();
             return;
         }
 
+        //TODO: Lock tree image. Also do this when TD player takes them from us.
         //Add tree to selected
         for (int i = 0; i < selectedProfiles.Length; i++)
         {
@@ -106,10 +128,25 @@ public class DatingHandler : MonoBehaviour
             selectedProfiles[i].index = treeIndex;
             profilePictures[treeIndex].Highlight(true);
 
+            //Remove selected tree from list
+            profilePictures[treeIndex].gameObject.SetActive(false);
+
             break;
         }
 
         ShouldShowDatePrompt();
+    }
+
+    /// <summary>
+    /// Selected trees can be returned from the new buttons which exist in
+    /// the selected dating profiles.
+    /// </summary>
+    /// <param name="selectedProfileIndex"></param>
+    public void SelectedButtonDeselection(int selectedProfileIndex)
+    {
+        if (dateState == DateState.Date)
+            return;
+        SelectTree(selectedProfiles[selectedProfileIndex].index);
     }
 
     private void DisplayTreeProfile(TreeDateStatblock tree, TreeProfilePicture profilePic)
@@ -131,7 +168,8 @@ public class DatingHandler : MonoBehaviour
                 return;
             }
         }
-        datePrompt.SetActive(true);
+        //datePrompt.SetActive(true);
+        BeginDate();
     }
 
     /// <summary>
@@ -157,11 +195,7 @@ public class DatingHandler : MonoBehaviour
         DisplayTreeProfile(child2, profilePictures[tree2Index]);
 
         //Update dating profile pictures.
-        profilePictures[tree1Index].Highlight(false);
-        profilePictures[tree2Index].Highlight(false);
-        selectedProfiles[0].gameObject.SetActive(false);
-        selectedProfiles[1].gameObject.SetActive(false);
-        ShouldShowDatePrompt();
+        EndDateAndReturnDaters();
 
     }
 
@@ -201,5 +235,52 @@ public class DatingHandler : MonoBehaviour
             treeYoungling.backgroundPattern = tree2.backgroundPattern;
 
         return treeYoungling;
+    }
+
+    public void BeginDate()
+    {
+        dateState = DateState.Date;
+        dateMinigameParent.SetActive(true);
+        //Todo set emoji prompt
+        //Todo set emoji answers
+        correctAnswerBubbleIndex = Random.Range(0, dateAnswerBubble.Length);
+        for(int i = 0; i < dateAnswerBubble.Length; i++)
+        {
+            if(i == correctAnswerBubbleIndex)
+            {
+                dateAnswerBubble[i].emoji.color = new Color(1f, 0.6f, 0.5f);
+                continue;
+            }
+
+            dateAnswerBubble[i].emoji.color = Color.white;
+        }
+    }
+
+    private void AnswerDatePrompt(int answerIndex)
+    {
+        if(answerIndex == correctAnswerBubbleIndex)
+        {
+            TryDate();
+        }
+        else
+        {
+            //Update dating profile pictures.
+            EndDateAndReturnDaters();
+        }
+    }
+
+    private void EndDateAndReturnDaters()
+    {
+        int tree1Index = selectedProfiles[0].index;
+        int tree2Index = selectedProfiles[1].index;
+        profilePictures[tree1Index].Highlight(false);
+        profilePictures[tree2Index].Highlight(false);
+        profilePictures[tree1Index].gameObject.SetActive(true);
+        profilePictures[tree2Index].gameObject.SetActive(true);
+        selectedProfiles[0].gameObject.SetActive(false);
+        selectedProfiles[1].gameObject.SetActive(false);
+
+        dateState = DateState.SelectTree;
+        dateMinigameParent.SetActive(false);
     }
 }
